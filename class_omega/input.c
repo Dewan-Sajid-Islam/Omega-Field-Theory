41f6954e1826e464a578036a57616c4f04d332a7
Dewan Sajid Islam <111456935+Dewan-Sajid-Islam@users.noreply.github.com>
2026-01-28 17:47:10 +0600

    input.c

---
+++ class_omega/input.c
@@ -0,0 +1,10 @@
+#ifndef INPUT_OMEGA_H
+#define INPUT_OMEGA_H
+
+int input_read_parameters_omega(
+    struct file_content *pfc,
+    struct precision *ppr,
+    struct background *pba
+);
+
+#endif
